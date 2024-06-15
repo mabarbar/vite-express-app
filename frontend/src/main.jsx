8bb05f3c63c629cb3886c851e6396d5d7cff3ddc
mabarbar <mateusz.barzenc00@gmail.com>
2024-06-15 20:13:48 +0200

    feat: full app

---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { WorkoutsContextProvider } from "./context/WorkoutsContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <WorkoutsContextProvider>
+            <App />
+        </WorkoutsContextProvider>
+    </React.StrictMode>
+);

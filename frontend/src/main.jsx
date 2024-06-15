33c47d6eab05bdc54897a7fb67b1f2f5c501b240
mabarbar <mateusz.barzenc00@gmail.com>
2024-06-15 22:59:06 +0200

    feat: added auth context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { WorkoutsContextProvider } from "./context/WorkoutsContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <WorkoutsContextProvider>
-            <App />
+            <AuthContextProvider>
+                <App />
+            </AuthContextProvider>
         </WorkoutsContextProvider>
     </React.StrictMode>
 );
